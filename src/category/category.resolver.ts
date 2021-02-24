84de2565fa0ac1994a835dfb717cd3dd141b81ac
Felipe <Felipe.dom.martins@gmail.com>
2021-02-23 21:10:58 -0300

    Create Category

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -1,12 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { Category } from './category.entity'
+import { CategoryMapper } from './category.mapper'
 import { CategoryService } from './category.service'
-import { Category } from './dto/category'
+import { CategoryPublic } from './dto/category'
+import { CategoryCreateInput } from './dto/category-create.input'
 
-@Resolver(of => Category)
+@Resolver(of => CategoryPublic)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
-  @Query(returns => [Category], { name: 'getAllCategories' })
-  async getAllCategories(): Promise<Category[]> {
+  @Query(returns => [CategoryPublic], { name: 'getAllCategories' })
+  async getAllCategories(): Promise<CategoryPublic[]> {
     return await this.categoryService.findAll()
+  }
+
+  @Mutation(returns => CategoryPublic, { name: 'createCategory' })
+  async createCategory(
+    @Args('input') input: CategoryCreateInput,
+  ): Promise<CategoryPublic> {
+    return this.categoryService.create(CategoryMapper.toEntity(input))
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Category } from './category.entity'
-import { Category as CategoryDTO } from './dto/category'
 
 @Injectable()
 export class CategoryService {
@@ -10,7 +9,11 @@ export class CategoryService {
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
   ) {}
-  async findAll(): Promise<CategoryDTO[]> {
+  async findAll(): Promise<Category[]> {
     return this.categoryRepository.find()
+  }
+
+  async create(input: Category): Promise<Category> {
+    return this.categoryRepository.save(input)
   }
 }
